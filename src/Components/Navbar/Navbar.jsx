5a01234940a318464720f40c723945a2cb63e0c3
jatingupta <jatingupta2306@gmail.com>
2024-02-08 14:28:08 +0530

    changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -10,29 +10,29 @@ import {
 import { Link } from "react-router-dom";
 import "./Nav.css"
 function Navbar() {
-const [list , setlist]=useState()
+  const [list, setlist] = useState()
   return (
     <AppBar position="static">
       <Toolbar>
-       <Grid container sx={{placeItems:"center"}}>
-         <Grid item xs={2}>
-      <Typography>
-        <Link to="/">
-          CrudApp
-        </Link>
-      </Typography>
-         </Grid>
-         <Grid item xs={4}/>
-         <Grid xs={6}>
-           <Tabs value={list} indicatorColor="primary" onChange={(e,val)=>setlist(val)}>
-           <Tab label={<Link to="/">Home</Link>} />
-           <Tab label={<Link to="/add">Add User</Link>} />
-           <Tab label={<Link to="/view">ViewUser</Link>} />
-           <Tab label={<Link to="/view">Update User</Link>} />
-           <Tab label={<Link to="/view">DeleteUser</Link>} />
-           </Tabs>
-       </Grid>
-       </Grid>
+        <Grid container sx={{ placeItems: "center" }}>
+          <Grid item xs={2}>
+            <Typography>
+              <Link to="/">
+                CrudApp
+              </Link>
+            </Typography>
+          </Grid>
+          <Grid item xs={4} />
+          <Grid xs={6}>
+            <Tabs value={list} indicatorColor="primary" onChange={(e, val) => setlist(val)}>
+              <Tab label={<Link to="/">Home</Link>} />
+              <Tab label={<Link to="/add">Add User</Link>} />
+              <Tab label={<Link to="/view">ViewUser</Link>} />
+              <Tab label={<Link to="/view">Update User</Link>} />
+              <Tab label={<Link to="/view">DeleteUser</Link>} />
+            </Tabs>
+          </Grid>
+        </Grid>
       </Toolbar>
     </AppBar>
   );
